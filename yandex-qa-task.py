8d6e9fb6b0bc2d52aed2fb513f84c09f982d27a6
Timofey <timayakovishin@gmail.com>
2022-11-12 15:41:43 +0300

    Задание сделано

---
+++ yandex-qa-task.py
@@ -0,0 +1,21 @@
+
+def getWordMaxRepeadInput() -> str:
+    """При запуске функции срабатывает функция input. После ввода создаётся лист. 
+    Применена функция фильтр, чтобы не трекать лишние пробелы за слова"""
+    inp = input("enter something: ")
+    tempList = list(filter(None, inp.split()))
+    tempDict = {}
+    for x in set(tempList):
+        tempDict[x] = tempList.count(x)
+    return max(tempDict, key=tempDict.get)
+
+
+
+
+if __name__ == '__main__':
+    # Данные для теста
+    # ввод: гора гора гора; вывод: гора
+    # ввод: река гора небо гора; вывод: гора 
+    print(getWordMaxRepeadInput()) 
+
+
